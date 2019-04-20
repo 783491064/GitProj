adca219a435d019b9cf27d8dc1945ddb57ac8b9c
bijingcun <13521319520@163.com>
2019-04-20 18:14:45 +0800

    Merge branch 'master' of https://github.com/783491064/GitProj
    # Conflicts:
    #	app/src/main/java/com/example/administrator/myapplication/MainActivity.java

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -14,10 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = findViewById(R.id.tv);
-        int a = 10;
-        int c = 30;
-        int b = 20;
-        int aaaaa = 30;
-        String s = "";
     }
 }
