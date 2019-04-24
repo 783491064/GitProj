49f10264dcf69d38f602354071896ec572295d66
bijingcun <13521319520@163.com>
2019-04-24 18:33:08 +0800

    firstcommit

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int b=2;
+        int hfc=9;
     }
 
 }
